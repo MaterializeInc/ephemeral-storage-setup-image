4022a5f40eb8156ea6357aff4209c87ba38a5212
Alex Hunt <alex.hunt@materialize.com>
2025-07-29 10:22:07 +0100

    Make sysctl values configurable

--- src/main.rs
+++ src/main.rs
@@ -24,6 +24,15 @@ enum Commands {
     Swap {
         #[clap(flatten)]
         common_args: CommonArgs,
+
+        #[arg(long, env, default_value_t = 100)]
+        vm_swappiness: usize,
+
+        #[arg(long, env, default_value_t = 1048576)]
+        vm_min_free_kbytes: usize,
+
+        #[arg(long, env, default_value_t = 100)]
+        vm_watermark_scale_factor: usize,
     },
 }
 
@@ -63,7 +72,12 @@ fn main() {
                     .setup(),
                 )
         }
-        Commands::Swap { common_args } => {
+        Commands::Swap {
+            common_args,
+            vm_swappiness,
+            vm_min_free_kbytes,
+            vm_watermark_scale_factor,
+        } => {
             let disk_detector = DiskDetector::new(commander.clone(), common_args.cloud_provider);
             tokio::runtime::Builder::new_current_thread()
                 .enable_all()
@@ -75,6 +89,9 @@ fn main() {
                         disk_detector,
                         node_name: common_args.node_name,
                         taint_key: common_args.taint_key,
+                        vm_swappiness,
+                        vm_min_free_kbytes,
+                        vm_watermark_scale_factor,
                     }
                     .setup(),
                 )
